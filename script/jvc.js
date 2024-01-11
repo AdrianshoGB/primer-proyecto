3062c0fa5d1e244e702ad6c7d8a8acfebeb21daa
Joel Vibert <vi02222148@zegelipae.pe>
2024-01-10 20:44:17 -0500

    Formularios: cambios de estilo de labeles + bonton enviar

--- script/jvc.js
+++ script/jvc.js
@@ -1,28 +1,84 @@
 function sendComment() {
-	let name = document.getElementById('name');
-	let email = document.getElementById('email');
-	let textInput = document.getElementById('text-input');
-	let mobileNumber = 51944502272;    /* Mobile number here [currently: Joel's] */
+    "use strict";
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let textInput = document.getElementById('text-input');
+    let mobileNumber = 51944502272;    /* Mobile number here [currently: Joel's] */
 
-	if (name.value.trim() == "")
+    if (name.value.trim() == "")
     {
-		name.style.background = "lightpink";
-		name.style.border = "4px solid red";
-		alert('Por favor ingresa su nombre');
-		return false;
-	}
-	if (email.value.trim() == "")
+        name.style.background = "lightpink";
+        name.style.border = "4px solid red";
+        alert('Por favor ingresa su nombre');
+        return false;
+    }
+    if (email.value.trim() == "")
     {
-		email.style.background = "lightpink";
-		email.style.border = "4px solid red";
-		alert('Por favor ingresa su correo electronico');
-		return false;
-	}
+        email.style.background = "lightpink";
+        email.style.border = "4px solid red";
+        alert('Por favor ingresa su correo electronico');
+        return false;
+    }
 
-	let url = `https://wa.me/${mobileNumber}?text=` + "Name: " + name.value + "%0a" + "Email ID: " + email.value + "%0a" + "Message: " + textInput.value;
+    let url = `https://wa.me/${mobileNumber}?text=` + "Name: " + name.value + "%0a" + "Email ID: " + email.value + "%0a" + "Message: " + textInput.value;
 
-	window.open(url, '_blank').focus();
+    window.open(url, '_blank').focus();
 }
+
+function sendConsulta() {
+    "use strict";
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let textInput = document.getElementById('text-input');
+    let mobileNumber = 51944502272;    /* Mobile number here [currently: Joel's] */
+
+    if (name.value.trim() == "")
+    {
+        name.style.background = "lightpink";
+        name.style.border = "4px solid red";
+        alert('Por favor ingresa su nombre');
+        return false;
+    }
+    if (email.value.trim() == "")
+    {
+        email.style.background = "lightpink";
+        email.style.border = "4px solid red";
+        alert('Por favor ingresa su correo electronico');
+        return false;
+    }
+
+    let url = `https://wa.me/${mobileNumber}?text=` + "Name: " + name.value + "%0a" + "Email ID: " + email.value + "%0a" + "Message: " + textInput.value;
+
+    window.open(url, '_blank').focus();
+}
+
+function sendAsesoramiento() {
+    "use strict";
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let textInput = document.getElementById('text-input');
+    let mobileNumber = 51944502272;    /* Mobile number here [currently: Joel's] */
+
+    if (name.value.trim() == "")
+    {
+        name.style.background = "lightpink";
+        name.style.border = "4px solid red";
+        alert('Por favor ingresa su nombre');
+        return false;
+    }
+    if (email.value.trim() == "")
+    {
+        email.style.background = "lightpink";
+        email.style.border = "4px solid red";
+        alert('Por favor ingresa su correo electronico');
+        return false;
+    }
+
+    let url = `https://wa.me/${mobileNumber}?text=` + "Name: " + name.value + "%0a" + "Email ID: " + email.value + "%0a" + "Message: " + textInput.value;
+
+    window.open(url, '_blank').focus();
+}
+
 
 /* for the carousel */
 let items = document.querySelectorAll('.carousel .carousel-item')
@@ -33,8 +89,8 @@ items.forEach((el) => {
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
-      	}
+            next = items[0]
+        }
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
